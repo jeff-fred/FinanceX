2799f8fafb30dcafcb1dbbe25beae410922a209a
jeff-fred <jefferyfrederic@gmail.com>
2020-12-31 11:10:05 -0700

    MAIN Application

---
+++ main.py
@@ -0,0 +1,28 @@
+''' Application to track your finances and view the latest data on them. '''
+
+# Imports
+import sys, os
+
+from data_manager import DatabaseHandling
+from UI_manager import MainWindow, RemoveTransactionWindow
+from UI_manager import AddTransactionWindow
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QTableWidgetItem
+
+
+
+if __name__ == '__main__':
+    # Application creation 
+    application = QApplication(sys.argv)
+
+    # Window Objects
+    mainWindow = MainWindow()
+    incomeRemWindow = RemoveTransactionWindow('Income')
+    expensesRemWindow = RemoveTransactionWindow('Expenses')
+
+    mainWindow.incomeRemoveButton.clicked.connect(incomeRemWindow.show)
+    mainWindow.expenseRemoveButton.clicked.connect(expensesRemWindow.show)
+
+    mainWindow.show()
+    sys.exit(application.exec_())
