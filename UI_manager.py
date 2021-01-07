7e4cb5bea706b34b6044140bbebf18f7028eb5b9
jeff-fred <jefferyfrederic@gmail.com>
2021-01-06 17:48:55 -0700

    Fixed transaction objects

--- UI_manager.py
+++ UI_manager.py
@@ -4,10 +4,12 @@
 import sys, os
 
 from data_manager import DatabaseHandling
+from data_manager import Transaction
 
 from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QTableWidgetItem, QListWidgetItem
+from PyQt5.QtWidgets import QAbstractItemView
 
 
 # Database Initialization to use with windows
@@ -28,13 +30,11 @@ class MainWindow(QMainWindow):
         self.expenseTable = windowUI.expensesTable
         self.incomeTable = windowUI.incomeTable
 
-        
         self.resize_table(self.incomeTable)
         self.resize_table(self.expenseTable)
-        
+
         self.load_data(self.incomeTable, db.return_all('Income'))
         self.load_data(self.expenseTable, db.return_all('Expenses'))
-
         
 
     # Resize table columns > None
@@ -43,19 +43,21 @@ class MainWindow(QMainWindow):
         tableObject.setColumnWidth(1, 70)
         tableObject.setColumnWidth(2, 85)
 
-    
-    # Load the data into the table columns > None
-    def load_data(self, tableObject, data):
-        tableObject.setRowCount(len(data))
+
+    # Load data into tables
+    def load_data(self, tableObj, data):
+        tableObj.setRowCount(len(data))
         row = 0
-        for i in data:
-            tableObject.setItem(row, 0, QTableWidgetItem(str(data[row][0])))
-            tableObject.setItem(row, 1, QTableWidgetItem(str(data[row][1])))
-            tableObject.setItem(row, 2, QTableWidgetItem(str(data[row][2])))
-            tableObject.setItem(row, 3, QTableWidgetItem(str(data[row][3])))
+        for item in data:
+            trans = Transaction(item[0], item[1], item[2], item[3])
+
+            tableObj.setItem(row, 0, trans.get_id())
+            tableObj.setItem(row, 1, trans.get_date())
+            tableObj.setItem(row, 2, trans.get_type())
+            tableObj.setItem(row, 3, trans.get_amount())
+
             row += 1
-
-
+    
 
 class RemoveTransactionWindow(QMainWindow):
     ''' GUI to allow user to remove transactions from table. '''
@@ -71,37 +73,21 @@ class RemoveTransactionWindow(QMainWindow):
         self.cancelButton = windowUI.cancelRemovalButton
         self.currentTable = windowUI.currentTransTable
         self.removalTable = windowUI.remTransTable
-
         self.widget_setup()
+
 
         self.resize_table(self.currentTable)
         self.resize_table(self.removalTable)
+
         self.load_data(self.currentTable, db.return_all(tableName))
 
-    
-    # Widget connection
+
+    # Setup and connect widgets
     def widget_setup(self):
-        self.cancelButton.clicked.connect(self.hide)
-        self.confirmButton.clicked.connect(self.confirm_removal)
-        self.removeButton.clicked.connect(self.removal_move)
-        self.keepButton.clicked.connect(self.keep_move)
-
-
-    # Move selected widgets to removal table > None
-    def removal_move(self):
-        pass
-        
-
-    # Move selected widgets back to current trans table > None     
-    def keep_move(self):
-        pass
-
-
-    # Remove from MAIN table and exit window
-    def confirm_removal(self):
-        pass
-    
-
+        self.removeButton.clicked.connect(self.remove_move)
+        self.currentTable.setSelectionMode(QAbstractItemView.SelectionMode(1))
+        self.currentTable.setSelectionBehavior(QAbstractItemView.SelectionBehavior(1))
+   
     # Resize table columns > None
     def resize_table(self, tableObject):
         tableObject.setColumnWidth(0, 40) 
@@ -109,16 +95,24 @@ class RemoveTransactionWindow(QMainWindow):
         tableObject.setColumnWidth(2, 118)
 
 
-    # Load tables with correct data > None
-    def load_data(self, tableObject, data):
-        tableObject.setRowCount(len(data))
+    # Load data onto table
+    def load_data(self, tableObj, data):
+        tableObj.setRowCount(len(data))
         row = 0
-        for i in data:
-            tableObject.setItem(row, 0, QTableWidgetItem(str(data[row][0])))
-            tableObject.setItem(row, 1, QTableWidgetItem(str(data[row][2])))
-            tableObject.setItem(row, 2, QTableWidgetItem(str(data[row][3])))
+        for item in data:
+            trans = Transaction(item[0], item[1], item[2], item[3])
+
+            tableObj.setItem(row, 0, trans.get_id())
+            tableObj.setItem(row, 1, trans.get_type())
+            tableObj.setItem(row, 2, trans.get_amount())
+
             row += 1
 
+
+    # Move row from current into removal table
+    def remove_move(self):
+        print(self.currentTable.selectedItems())
+    
 
 class AddTransactionWindow(QMainWindow):
     ''' GUI to allow user to input a new transaction. '''
